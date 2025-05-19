edcef9e5f551475123610de129b9cd7aca06b65b
rahim707 <mazouzceminfo@gmail.com>
2025-05-19 21:51:17 +0200

    Updated my message

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return  {"message": "Hello, my docker and my cd/ci!"}
+    return  {"message": "Hello, my docker and my fully cd/ci!"}
 
 @app.get("/square/{number}")
 async def square_number(number: int):
